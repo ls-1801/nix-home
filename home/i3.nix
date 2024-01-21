11541799d83440dde163f2e7d01c876ba0a705f1
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-01-21 22:10:11 +0100

    I3: move workspace to next output

--- home/i3.nix
+++ home/i3.nix
@@ -7,10 +7,12 @@
       modifier = "Mod4";
       keybindings = let modifier = config.xsession.windowManager.i3.config.modifier;
       in lib.mkOptionDefault {
-        "Mod4+Return" = "exec /usr/bin/kitty";
-        "Mod4+Shift+Return" = "exec /usr/bin/firefox";
-        "Mod4+q" = "kill";
-        "Mod4+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
+        "${modifier}+Return" = "exec /usr/bin/kitty";
+        "${modifier}+Shift+Return" = "exec /usr/bin/firefox";
+        "${modifier}+q" = "kill";
+        "${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun";
+
+        "${modifier}+less" = "move workspace to output left";
       };
     };
   };
