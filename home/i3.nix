cdf5f234b952ac6d084ba5aa1e5339a7884c7e64
Lukas Schwerdtfeger <lukas.schwerdtfeger@gmail.com>
2024-01-21 22:29:30 +0100

    Polybar

--- home/i3.nix
+++ home/i3.nix
@@ -1,10 +1,43 @@
 { pkgs, config, lib, ... }:
 
- {
+{
+  
+  services.polybar = {
+    enable = true;
+    script = "polybar top &";
+    config = {
+      "bar/top" = {
+        monitor = "\${env:MONITOR:DP-1}";
+        width = "100%";
+        height = "3%";
+        radius = 0;
+        modules-center = "date";
+      };
+
+      "module/date" = {
+        type = "internal/date";
+        internal = 5;
+        date = "%d.%m.%y";
+        time = "%H:%M";
+        label = "%time%  %date%";
+      };
+    };
+  };
+
+  xsession = {
+    enable = true;
+    initExtra = ''
+      setxkbmap de -variant nodeadkeys -option caps:esc
+    '';
+  };
+
   xsession.windowManager.i3 = {
     enable = true;
     config = {
       modifier = "Mod4";
+      startup = [
+         { command = "systemctl --user restart polybar"; always = true; notification = false; }
+      ];
       keybindings = let modifier = config.xsession.windowManager.i3.config.modifier;
       in lib.mkOptionDefault {
         "${modifier}+Return" = "exec /usr/bin/kitty";
